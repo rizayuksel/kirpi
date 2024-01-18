e6dffa4e6babd41c85ff04683a5dc02a8eb9556a
rizayuksel <riza@olamaz.com>
2024-01-18 21:50:09 +0300

    Added pdf

--- kirpi/trailers/urls.py
+++ kirpi/trailers/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("v1/trailers/", views.ListCreateTrailersView.as_view(), name="list_trailers"),
     path("v1/trailers/detail/<str:pk>/", views.DetailTrailerView.as_view(), name="trailer_detail"),
     path("v1/trailers/count/", views.TrailerCountView.as_view(), name="trailer_count"),
+    path("v1/trailers/bill/<str:pk>/", views.TrailerBillView.as_view(), name="trailer_bill"),
 ]
--- kirpi/trailers/views.py
+++ kirpi/trailers/views.py
@@ -1,16 +1,26 @@
 from datetime import datetime
 from decimal import Decimal
+from io import BytesIO
 from typing import TYPE_CHECKING
 
 from kirpi.paramaters.models import Price
 from kirpi.trailers.models import Trailer
 from kirpi.trailers.serializers import DetailTrailerSerializer, ListTrailersSerializer
 
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
+
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework.views import APIView
+
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+
 
 if TYPE_CHECKING:
     from django.db.models.query import QuerySet
@@ -63,3 +73,42 @@ class TrailerCountView(APIView):
     def get(self, request, format=None):
         trailer_count = Trailer.objects.filter().count()
         return Response({"trailer_count": trailer_count}, status=HTTP_200_OK)
+
+
+class TrailerBillView(View):
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs["pk"]
+        trailer = get_object_or_404(Trailer, id=pk)
+        
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{trailer.serial_number}_{datetime.now().date()}.pdf"'
+
+        pdf_buffer = BytesIO()
+        pdf = SimpleDocTemplate(pdf_buffer, pagesize=letter)
+
+        table_data = [
+            ["SERI NUMARASI", trailer.serial_number],
+            ["ÖNCEKI SAYAÇ ÖLÇÜM TARIHI", str(trailer.last_date.date())],
+            ["ÖNCEKI SAYAÇ ÖLÇÜM DEGERI", trailer.last_t0],
+            ["SON SAYAÇ ÖLÇÜM TARIHI", str(trailer.date.date())],
+            ["SON SAYAÇ ÖLÇÜM DEGERI", trailer.t0],
+            ["FIYAT", trailer.price],
+        ]
+
+        table = Table(table_data, colWidths=[200, 200])
+        style = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.bisque),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.lightskyblue),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 30),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.lightgrey),
+        ])
+        table.setStyle(style)
+
+        pdf.build([table])
+
+        pdf_buffer.seek(0)
+        response.write(pdf_buffer.read())
+
+        return response
